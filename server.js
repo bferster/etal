a8ea1f2cd64cace5e09137781cc0431e2dd3e24d
bferster <bferster@stagetools.com>
2020-09-03 11:51:38 -0400

    chat shell

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 
-console.log("Hello from Node!");
+	console.log("Hello from Node!");
+
+	const WebSocket = require('ws');
+
+	const webSocketServer = new WebSocket.Server({ port: 8080 });
+		webSocketServer.on('connection', (webSocket) => {
+		webSocket.on('message', (message) => {
+			console.log('Received:', message);
+			broadcast(message);
+			});
+		});
+
+		function broadcast(data) {
+			webSocketServer.clients.forEach((client) => {
+				if (client.readyState === WebSocket.OPEN) {
+				client.send(data);
+			}
+		});
+		}
