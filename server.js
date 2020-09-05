6502c7bce7e1fd5154505a0c755f1c2a009f538d
bferster <bferster@stagetools.com>
2020-09-05 14:54:31 -0400

    stuff

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 
 	console.log("Initializing nodeJS server");
 	const WebSocket = require('ws');
-	const http=require('http');
-    const fs=require('fs');
-    const url=require('url');
+	const AWS = require("aws-sdk");
+	AWS.config.loadFromPath('./config.json');
+
+// PEOPLE
+
+	var docClient=new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+	var params={ TableName: 'people', Key: { "meeting": 2 }};
+	
+/*	docClient.get(params, (err, data)=> {
+		if (err) 	{ console.log("Error", err); } 
+		else 		{ console.log("Success", data.Item); };
+		});
+*/
+	var params2={ TableName: 'people', Item: { "id":1 , data:"data"}};
+	
+	docClient.put(params2, (err, data)=> {
+        if (err) 	{ console.error("Unable to write data: ", JSON.stringify(err, null, 2)); }
+        else 	 	{ console.log("PutItem succeeded"); }
+		});
+
 
 // WEBSOCKET SERVER
 
@@ -27,7 +44,8 @@
 
 // HTTP SERVER
 		
-	const server=http.createServer((req, res)=>{
+/*		const http=require('http');
+		const server=http.createServer((req, res)=>{
 		let v=req.url.split("/")
 		trace(v,req.url)
 		if (v[2] == "people") {
@@ -42,28 +60,11 @@
 	 	});
 
 	server.listen(8001);
+*/
 	console.log("server initialized on port 8001");
 
 
 // HELPERS
-
-	function ParseCSV(str) {															// PARSE CSV TO NESTED ARRAYS
-		var arr=[];
-		var quote=false;  																	// True means we're inside a quoted field
-		str=str.replace(/\\r\\n/g,"\n");													// Convert \r\n -> \n
-		str=str.replace(/\\n\\r/g,"\n");													// Convert \n\r -> \n
-		for (var row=col=c=0;c < str.length;c++) {   										// Iterate over each character, keep track of current row and column (of the returned array)
-			var cc=str[c],nc=str[c+1];        												// Current character, next character
-			arr[row]=arr[row] || [];             											// Create a new row if necessary
-			arr[row][col]= arr[row][col] || '';   											// Create a new column (start with empty string) if necessary
-			if (cc == '"' && quote && nc == '"') { arr[row][col] += cc; ++c; continue; } 	// If the cur char is ", and we're inside a quoted field, and the next char is also a ", add a " to the current column and skip the next character
-			if (cc == '"') { quote = !quote; continue; }        							// If it's just one quotation mark, begin/end quoted field
-			if (cc == ',' && !quote) { ++col; continue; }      								// If it's a comma and we're not in a quoted field, move on to the next column
-			if (cc == '\n' && !quote) { ++row; col = 0; continue; }     					// If it's a newline and we're not in a quoted field, move on to the next row and move to column 0 of that new row
-			arr[row][col] += cc;        													// Otherwise, append the current character to the current column
-			}
-		return arr;																			// Return nested arrays
-		}
 
 	function trace(msg, p1, p2, p3, p4)									
 	{
