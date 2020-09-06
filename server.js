d6a884d840037b48d9236a17147037c0e89f515f
bferster <bferster@stagetools.com>
2020-09-06 13:17:09 -0400

    new chat, people loading

--- server.js
+++ server.js
@@ -3,65 +3,64 @@
 	const WebSocket = require('ws');
 	const AWS = require("aws-sdk");
 	AWS.config.loadFromPath('./config.json');
-
-// PEOPLE
-
 	var docClient=new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-	var params={ TableName: 'people', Key: { "meeting": 2 }};
-	
-/*	docClient.get(params, (err, data)=> {
-		if (err) 	{ console.log("Error", err); } 
-		else 		{ console.log("Success", data.Item); };
-		});
-*/
-	var params2={ TableName: 'people', Item: { "id":1 , data:"data"}};
-	
-	docClient.put(params2, (err, data)=> {
-        if (err) 	{ console.error("Unable to write data: ", JSON.stringify(err, null, 2)); }
-        else 	 	{ console.log("PutItem succeeded"); }
-		});
-
 
 // WEBSOCKET SERVER
 
 	const webSocketServer = new WebSocket.Server({ port:8080 });
-	
+
 	webSocketServer.on('connection', (webSocket) => {
-		console.log("Initializing websocket server on port 8080");
+		console.log("New connect");
+		webSocket.myId=0;
 		webSocket.on('message', (message) => {
 			console.log('Received:', message);
-			broadcast(message);
+			if (!message)	return;
+			let v=message.split("|");
+			if (!webSocket.myId) 	webSocket.myId=v[1];
+			if (v[0] == "C") 		Chat(v[1],v[2],v[3]);
+			else if (v[0] == "B") 	Broadcast(v[1],v[2]);
+			else if (v[0] == "P") 	SendPeople(v[1],v[2]);
 			});
 		});
 
-	function broadcast(data) {
+	function Broadcast(id, msg)
+	{
+		let str="C|"+id+"|*|"+msg;
 		webSocketServer.clients.forEach((client) => {
-			if (client.readyState === WebSocket.OPEN) {
-			client.send(data);
-			}
+			if (client.readyState === WebSocket.OPEN) 	client.send(str);
+			});
+	}
+	
+	function Send(id, data) 
+	{
+		webSocketServer.clients.forEach((client) => {
+			if (client.myId == id)
+				if (client.readyState === WebSocket.OPEN) 
+					client.send(data);
 			});
 		}
+	
+	function Chat(fromId,toId, msg) 
+	{
+		let str="C|"+toId+"|"+fromId+"|"+msg;
+		trace(fromId, toId, msg) 
+		webSocketServer.clients.forEach((client) => {
+			if (client.myId == toId)
+				if (client.readyState === WebSocket.OPEN) 
+					client.send(str);
+			});
+		}
+	
+	function SendPeople(id)	
+	{
+		var params = { TableName : 'people', FilterExpression : 'meeting = :q',ExpressionAttributeValues : {':q' :id.split("~")[0]}  };
+		docClient.scan(params, (err, data)=> {
+			if (err) 	{ console.error("Unable to read data: ", JSON.stringify(err, null, 2)); }
+			else 		{ console.log("People sent to "+id); Send(id,"P|"+JSON.stringify(data.Items)); };
+		});
+	
+	}
 
-// HTTP SERVER
-		
-/*		const http=require('http');
-		const server=http.createServer((req, res)=>{
-		let v=req.url.split("/")
-		trace(v,req.url)
-		if (v[2] == "people") {
-			let f="./meetings/"+v[1]+"-"+v[2]+".csv";
-				fs.readFile(f, "utf8", (err, data)=>{
-				if (err) {	return console.log(err);  }
-				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.write("PEOPLE("+JSON.stringify(ParseCSV(data))+")");
-				res.end();
-				});
-			}
-	 	});
-
-	server.listen(8001);
-*/
-	console.log("server initialized on port 8001");
 
 
 // HELPERS
@@ -79,3 +78,4 @@
 		else
 			console.log(msg);
 	}
+	
